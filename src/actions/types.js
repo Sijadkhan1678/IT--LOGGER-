56c2462e4a3799f5f158713b6344a78707e67997
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-03-13 15:44:42 +0500

    some changes in types.js and logActions.js

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,9 +1,9 @@
 export const ADD_LOG= 'ADD_LOG';
 export const GET_LOGS= 'GET_LOGS';
-export const UPDATE_LOG= 'ADD_LOG'
-export const DELETE_LOG= 'ADD_LOG'
+export const UPDATE_LOG= 'UPDATE_LOG'
+export const DELETE_LOG= 'DELETE_LOG'
 export const LOG_ERROR= 'LOG_ERROR'
-export const ADD_LOG= 'ADD_LOG'
-export const ADD_LOG= 'ADD_LOG'
-export const ADD_LOG= 'ADD_LOG'
-export const SET_LOADING= 'SET_LOADING';
+export const SET_LOADING= 'SET_LOADING'
+
+
+
